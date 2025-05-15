10fb56b18362e7fead3b90ff53130350b2e8834a
xinalaw <126695685+xinalaw@users.noreply.github.com>
2025-05-16 02:08:18 +0300

    Часть функционала перенесена в другой файл

--- src/main.js
+++ src/main.js
@@ -1,29 +1,20 @@
 import HeaderComponent from './view/header-component.js';
 import FormAddTaskComponent from './view/task-input-component.js';
-import {render, RenderPosition} from './framework/render.js';
-import TaskBoardComponent from './view/taskboard-component.js';
-import TaskListComponent from './view/task-list-component.js';
-import TaskComponent from './view/task-component.js';
+import { render, RenderPosition } from './framework/render.js';
+import TasksBoardPresenter from './presenter/tasks-board-presenter.js';
+import TasksModel from './model/task-model.js';
+
+
+
+const tasksModel = new TasksModel();
 
 const bodyContainer = document.querySelector('.board-app');
 const formContainer = document.querySelector('.new-task');
 const mainContainer = document.querySelector('.main');
+
+const taskBoardPresenter = new TasksBoardPresenter({ boardContainer: mainContainer,tasksModel });
+
 render(new HeaderComponent(), bodyContainer, RenderPosition.BEFOREBEGIN);
 render(new FormAddTaskComponent(), formContainer);
-render(new TaskBoardComponent(), mainContainer, RenderPosition.BEFOREEND);
 
-const taskContainer = document.querySelector('.taskboard');
-
-
-for (let i = 0; i < 4; i++) {
-    const taskListComponent = new TaskListComponent();
-    render(taskListComponent, taskContainer);
-  
-   
-    const taskListElement = taskListComponent.getElement().querySelector('.task-list');
-  
-    for (let j = 0; j < 4; j++) {
-      render(new TaskComponent(), taskListElement);
-    }
-  }
-  
+taskBoardPresenter.init();
