88024fb17e195345a4de3f61c25c4c04d94e50f5
xinalaw <126695685+xinalaw@users.noreply.github.com>
2025-05-16 01:57:52 +0300

    Добавлен файл с задачами отдельный

---
+++ src/mock/task.js
@@ -0,0 +1,12 @@
+export const tasks = [
+  { id: 1, title: 'Написать ТЗ', status: 'backlog' },
+  { id: 2, title: 'Создать компоненты', status: 'in-processing' },
+  { id: 3, title: 'Реализовать презентер', status: 'in-processing' },
+  { id: 4, title: 'Провести тестирование', status: 'backlog' },
+  { id: 5, title: 'Исправить баги', status: 'done' },
+  { id: 6, title: 'Залить в репозиторий', status: 'done' },
+  { id: 7, title: 'Написать документацию', status: 'done' },
+  { id: 8, title: 'Настроить сборку проекта', status: 'done' },
+  { id: 9, title: 'Создать релиз', status: 'trashcan' },
+  { id: 10, title: 'Обновить зависимости', status: 'in-processing' }
+];
